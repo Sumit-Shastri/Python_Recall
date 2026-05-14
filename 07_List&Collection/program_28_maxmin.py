b0f9af9930b9634a8079f8f0ca07db4b597e61b6
Sumit-Shastri <shastrisumit25@gmail.com>
2026-05-14 19:51:53 +0530

    program 27 : find max min from list

---
+++ 07_List&Collection/program_28_maxmin.py
@@ -0,0 +1,15 @@
+# find max / min from list
+
+def FindMaxMin(l):
+
+    maxEle = max(l)
+    minEle = min(l)
+
+    print("max of list is : ",maxEle)
+    print("min of list is : ",minEle)
+
+list = [25,67,78,12,34,667]
+FindMaxMin(list)
+
+
+
