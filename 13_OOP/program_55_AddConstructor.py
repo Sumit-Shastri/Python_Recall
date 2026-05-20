44e870f44d2cd71b1bfe5b8865e0b5c6ef400adc
Sumit-Shastri <shastrisumit25@gmail.com>
2026-05-20 16:30:29 +0530

    Add constructor

---
+++ 13_OOP/program_55_AddConstructor.py
@@ -0,0 +1,26 @@
+# constructor
+
+class Car:
+
+    def __init__():
+        print("---   Car   ---")
+
+    def __init__(self, userCarName, userCarSeat, userCarEngine):
+        self.carName = userCarName
+        self.carSeats = userCarSeat
+        self.carEngine = userCarEngine
+
+        print("car name : ", self.carName)
+        print("car seating : ", self.carSeats)
+        print("car Engine : ", self.carEngine)
+    
+    def start(self):
+        print("car started...")
+        print("rrrrrrrr")
+
+    def stop(self):
+        print("car stopped")
+
+userCar1 = Car("Maruti-800", 5, 800)
+userCar1.start()
+userCar1.stop()
